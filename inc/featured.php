37c59ce2e12cab0378eea2c4ddfb4a33f918d57f
Alexander Agnarson <alexander.agnarson@gmail.com>
2017-12-17 12:38:36 +0100

    Fix default featured count

--- inc/featured.php
+++ inc/featured.php
@@ -6,8 +6,8 @@ $featured = new WP_Query(
 		'update_post_meta_cache'	=> false,
 		'update_post_term_cache'	=> false,
 		'ignore_sticky_posts'		=> 1,
-		'posts_per_page'			=> get_theme_mod('featured-posts-count'),
-		'cat'						=> get_theme_mod('featured-category')
+		'posts_per_page'			=> get_theme_mod('featured-posts-count','6'),
+		'cat'						=> get_theme_mod('featured-category','')
 	)
 );
 ?>
