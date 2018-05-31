d4b379b0d704d379c0666e4be573258159915ae7
Alexander Agnarson <alexander.agnarson@gmail.com>
2018-05-31 19:28:27 +0200

    fix

--- functions.php
+++ functions.php
@@ -118,7 +118,6 @@ if ( ! function_exists( 'alx_scripts' ) ) {
 		wp_enqueue_script( 'blogrow-fitvids', get_template_directory_uri() . '/js/jquery.fitvids.js', array( 'jquery' ),'', true );
 		wp_enqueue_script( 'blogrow-owl-carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', array( 'jquery' ),'', true );
 		wp_enqueue_script( 'blogrow-scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ),'', true );
-		if ( is_singular() ) { wp_enqueue_script( 'blogrow-sharrre', get_template_directory_uri() . '/js/jquery.sharrre.min.js', array( 'jquery' ),'', true ); }
 		if ( is_singular() && get_option( 'thread_comments' ) )	{ wp_enqueue_script( 'comment-reply' ); }
 	}  
 	
