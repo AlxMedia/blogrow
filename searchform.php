fadc626384509d209ca2cd1d379a0d6e1ffd0d70
Alexander Agnarson <alexander.agnarson@gmail.com>
2018-06-25 15:43:56 +0200

    Change esc_html_e to esc_attr_e in searchform.php

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form method="get" class="searchform themeform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div>
-		<input type="text" class="search" name="s" onblur="if(this.value=='')this.value='<?php esc_html_e('To search type and hit enter','blogrow'); ?>';" onfocus="if(this.value=='<?php esc_html_e('To search type and hit enter','blogrow'); ?>')this.value='';" value="<?php esc_html_e('To search type and hit enter','blogrow'); ?>" />
+		<input type="text" class="search" name="s" onblur="if(this.value=='')this.value='<?php esc_attr_e('To search type and hit enter','blogrow'); ?>';" onfocus="if(this.value=='<?php esc_attr_e('To search type and hit enter','blogrow'); ?>')this.value='';" value="<?php esc_attr_e('To search type and hit enter','blogrow'); ?>" />
 	</div>
 </form>
