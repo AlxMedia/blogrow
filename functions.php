4a437c93084d7bdcf6f628d257fddc55720570ff
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-03-03 16:31:14 +0100

    1.2.1

--- functions.php
+++ functions.php
@@ -89,15 +89,17 @@ add_action( 'after_setup_theme', 'blogrow_setup' );
 
 /*  Custom navigation
 /* ------------------------------------ */
-require_once 'functions/nav.php';
-$nav = new \AlxMedia\Nav();
-$nav->enqueue(
-	[
-		'script' => 'js/nav.js',
-		'inline' => false,
-	]
-);
-$nav->init();
+add_action( 'wp', function() {
+	require_once 'functions/nav.php';
+	$nav = new \Blogrow\Nav();
+	$nav->enqueue(
+		[
+			'script' => 'js/nav.js',
+			'inline' => false,
+		]
+	);
+	$nav->init();
+} );
 
 
 /*  Custom logo
